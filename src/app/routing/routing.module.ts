cf653200a8b83a077219441577cc8d8cb4beb50c
Roy Chela <roychela@gmail.com>
2019-06-10 10:22:19 +0300

    Fix searchform import error at routing module

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './../profile/profile.component';
-import { SearchComponent } from './../searchform/searchform.component';
+import { SearchformComponent } from './../searchform/searchform.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'profile', component: ProfileComponent },
-  { path: 'search', component: SearchComponent },
+  { path: 'search', component: SearchformComponent },
   { path: '', redirectTo: 'search', pathMatch: 'full' }
 ]
 
