93202f61c40624b1f9cdc4e4f3027dec8be2ff05
Roy Chela <roychela@gmail.com>
2019-06-10 10:04:03 +0300

    Add profile and search routes

--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProfileComponent } from './../profile/profile.component';
+import { SearchComponent } from './../search/search.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'profile', component: ProfileComponent },
+  { path: 'search', component: SearchComponent },
+  { path: '', redirectTo: 'search', pathMatch: 'full' }
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   declarations: []
 })
 export class RoutingModule { }
