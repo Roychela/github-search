fe4e8d881b1c4915e4e75c4c8b10238cb32ff74d
Roy Chela <roychela@gmail.com>
2019-06-10 09:47:57 +0300

    Add form to searchform component

--- src/app/searchform/searchform.component.ts
+++ src/app/searchform/searchform.component.ts
@@ -1,13 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { Repo } from './../repo';
+import { RepoService } from '../repo.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
-  selector: 'app-searchform',
-  templateUrl: './searchform.component.html',
-  styleUrls: ['./searchform.component.css']
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
 })
-export class SearchformComponent implements OnInit {
+export class SearchComponent implements OnInit {
+  reponame: Repo;
+  user: any;
+  username: string;
+  repos: any;
+  //searchvalue: string;
+  repository: any;
 
-  constructor() { }
+
+  constructor(private RepoService: RepoService, private http: HttpClient) {
+  }
+
+  findProfile() {
+
+    this.RepoService.getProfileInfo(this.username).subscribe(result => {
+      this.user = result;
+      console.log(this.username)
+    });
+  }
 
   ngOnInit() {
   }
